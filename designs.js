a3b56a1fd468271fc9326cc371b8da178460a58a
mbarachi <mbarachivictor@gmail.com>
2018-07-01 17:56:36 +0100

    Get data from form

--- designs.js
+++ designs.js
@@ -1,22 +1,20 @@
 // Select color input
-// Select size input
+const colorPicker = $("#colorPicker").val();
 
 // When size is submitted by the user, call makeGrid()
+$("#sizePicker").submit(function (e) {
+	e.preventDefault();
+	makeGrid()
+})
 
 function makeGrid() {
 
 // Your code goes here!
-const height = document.getElementById("inputHeight").value;
-const weight = document.getElementById("inputWeight").value;
-document.getElementById("pixelCanvas").innerHTML = "<p></p>";
+const height = $("#inputHeight").val()
+const width = $("#inputWeight").val()
+const canvasSize = {height: height, width: width};
 
-	for (var i = 1; i <= height; i++) {
-		for (var j = 1; j <= weight; j++) {
-			node = document.createElement("td")
-			document.getElementById("pixelCanvas").appendChild(`<td id="${i}x${j}"></td>`);
-		}
-	}
-
+console.log(canvasSize)
 
 }
 
