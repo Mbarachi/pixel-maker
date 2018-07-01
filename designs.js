025ea233d0aab36738dd6cffbf484a549d42dca4
mbarachi <mbarachivictor@gmail.com>
2018-07-01 18:37:02 +0100

    add makeGrid function

--- designs.js
+++ designs.js
@@ -4,18 +4,27 @@ const colorPicker = $("#colorPicker").val();
 // When size is submitted by the user, call makeGrid()
 $("#sizePicker").submit(function (e) {
 	e.preventDefault();
-	makeGrid()
+	makeGrid();
 })
 
 function makeGrid() {
 
-// Your code goes here!
-const height = $("#inputHeight").val()
-const width = $("#inputWeight").val()
-const canvasSize = {height: height, width: width};
+	// Your code goes here!
+	$("table").html("")
 
-console.log(canvasSize)
+	const height = $("#inputHeight").val();
+	const width = $("#inputWeight").val();
+	const canvasSize = {height: height, width: width};
 
+	for (var i = canvasSize.height; i >= 1; i--) {
+		const row = document.createElement("tr");
+		row.setAttribute("id", i);
+		for (var j = canvasSize.width; j >= 1; j--) {
+			const id = i + "_" + j;
+			const td = document.createElement("td");
+			td.setAttribute("id", id);
+			row.prepend(td);
+		}
+		$("table").prepend(row);
+	}
 }
-
-
